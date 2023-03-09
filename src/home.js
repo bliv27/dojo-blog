83a073ef109c277113535be441a159fe0c92b04c
David <david.raffin@dincel.com.au>
2023-03-09 11:45:41 +1100

    lesson19 - fetch errors

--- src/home.js
+++ src/home.js
@@ -5,23 +5,33 @@ import BlogList from './BlogList';
 const Home = () => {
     const[blogs, setBlogs] = useState(null);
     const[isPending, setIsPending] = useState(true);
+    const[error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok){
+                    throw Error('could not fetch the data for that resource');
+                }
                 return res.json()
             })
-            .then((data) =>{
+            .then((data) => {
                 console.log(data)
                 setBlogs(data);
                 setIsPending(false);
-            });
+                setError(null);
+            })
+            .catch((err) =>{
+                setIsPending(false);
+                setError(err.message);
+            })
     }, []);
 
     return ( 
         <div className="home">
-            {isPending && <div>loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+            { error && <div>{ error }</div> }
+            { isPending && <div>loading...</div> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
         </div>
      );
 }
