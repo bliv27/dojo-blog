15e3c1709ee2a2252aed69f386255be3b7b549e8
David <david.raffin@dincel.com.au>
2023-03-09 14:26:47 +1100

    lesson30 - redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,8 +20,10 @@ const Create = () => {
             body: JSON.stringify(blog)
         }).then(() => {
             setIsPending(false);
-            console.log('new blog added')
+            history.push('/');
         })
+
+        
     }
 
     return ( 
