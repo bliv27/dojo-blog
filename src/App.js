61ef8fb84481f85e867a0edc75207d53f2949218
David <david.raffin@dincel.com.au>
2023-03-09 12:19:23 +1100

    lesson23 - router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import Navbar from './navbar';
 import Home from './home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch >
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
+    
   );
 }
 
